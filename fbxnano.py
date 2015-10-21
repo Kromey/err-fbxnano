c820dc48faa8d153315fdf189b7e1fdbf189fbc4
Travis Veazey <travisvz@gmail.com>
2015-10-21 11:55:23 -0800

    Add some character to the invite_me response

--- fbxnano.py
+++ fbxnano.py
@@ -1,6 +1,7 @@
 # This is a skeleton for Err plugins, use this to get started quickly.
 
 from errbot import BotPlugin, botcmd
+from errbot.backends.base import RoomNotJoinedError
 
 
 class FbxNano(BotPlugin):
@@ -22,7 +23,24 @@ class FbxNano(BotPlugin):
     @botcmd
     def invite_me(self, mess, args):
         """Ask the bot to invite you to the group chat"""
-        #Invite the user to the NANO_ROOM
-        self.query_room(self.config['NANO_ROOM']).invite(mess.frm.person)
-        return "You've been invited to the party!"
+        #Get the room object -- we're going to play with it a bit
+        room = self.query_room(self.config['NANO_ROOM'])
+
+        #Get the count of occupants in the room
+        try:
+            occupants = len(room.occupants)
+        except RoomNotJoinedError:
+            return "I cannot comply. I am not there."
+
+        #The bot is in the room as well, reduce the count
+        occupants = occupants - 1
+
+        #Now invite the user
+        room.invite(mess.frm.person)
+
+        #And finally, give some character in the response
+        if occupants:
+            return "The others might enjoy your presence."
+        else:
+            return "Very well, but you and I could converse just as well here."
 
